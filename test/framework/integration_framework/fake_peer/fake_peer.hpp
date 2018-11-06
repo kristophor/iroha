47c986c4386f6e5063802968572c7e4fbca2eb76
Mikhail Boldyrev <miboldyrev@gmail.com>
2018-11-06 17:19:42 +0300

    added loader funcs to behaviour
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/framework/integration_framework/fake_peer/fake_peer.hpp
+++ test/framework/integration_framework/fake_peer/fake_peer.hpp
@@ -82,6 +82,8 @@ namespace integration_framework {
       using OgProposalPtr = std::shared_ptr<shared_model::interface::Proposal>;
       using OsBatchPtr =
           std::shared_ptr<shared_model::interface::TransactionBatch>;
+      using LoaderBlockRequest = std::shared_ptr<shared_model::crypto::Hash>;
+      using LoaderBlocksRequest = shared_model::interface::types::HeightType;
 
       /**
        * Constructor.
